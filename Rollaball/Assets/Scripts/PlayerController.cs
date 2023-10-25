9be0748422960349a4eb37fcdc688252c6159131
Changjie (Sam) Xu <xcjsam@g.ucla.edu>
2023-10-24 20:07:18 -0700

    add new objects

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,15 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb; 
     private int count;
+    public float gameTime = 30;
+    private bool gameOver = false;
     private float movementX;
     private float movementY;
     public float speed = 0; 
     public TextMeshProUGUI countText;
+    public TextMeshProUGUI speedText;
+    public TextMeshProUGUI timerText;
+    public GameObject loseTextObject;
     public GameObject winTextObject;
 
     // Start is called before the first frame update
@@ -20,7 +25,10 @@ public class PlayerController : MonoBehaviour
         count = 0;
         rb = GetComponent<Rigidbody>(); 
         SetCountText();
+        SetSpeedText();
+        SetTimerText();
         winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
     }
 
     void OnMove (InputValue movementValue)
@@ -40,11 +48,33 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void SetSpeedText() 
+    {
+        speedText.text =  "Speed: " + speed.ToString();
+    }
+
+    void SetTimerText()
+    {
+        timerText.text = "Time: " + Mathf.Round(gameTime).ToString() + "s";
+    }
+    
     private void FixedUpdate() 
     {
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        if (!gameOver) {
+            Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
 
-        rb.AddForce(movement * speed);
+            rb.AddForce(movement * speed);
+
+            gameTime -= Time.deltaTime;
+            SetTimerText();
+
+            if (gameTime <= 0)
+            {
+                gameTime = 0;
+                gameOver = true;
+                loseTextObject.SetActive(true);
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other) 
@@ -59,6 +89,19 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             speed = speed * 2;
+            SetSpeedText();
+        }
+        if (other.gameObject.CompareTag("SpeedDown")) 
+        {
+            other.gameObject.SetActive(false);
+            speed = speed / 2;
+            SetSpeedText();
+        }
+        if (other.gameObject.CompareTag("Heal")) 
+        {
+            other.gameObject.SetActive(false);
+            speed = 10;
+            SetSpeedText();
         }
     }
 }
